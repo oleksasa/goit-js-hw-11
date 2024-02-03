1d08e07db851de8ef7a5e2aec11cad830eae83af
oleksasa <oleksandrasevcuk04@gmail.com>
2024-02-03 22:58:05 +0200

    fourth attempt

--- src/js/constants/classes.js
+++
@@ -1,1 +0,0 @@
-export const GALLERY_LINK = 'gallery-link';
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,13 +1,19 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import fetchImages from './api/pixabay'
-import refs from './constants/selectors'
-import createGallery from './templates/createGallery'
-import { GALLERY_LINK } from './constants/classes.js'
-import { toastError, toastSuccess } from './components/toast/toast';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-refs.searchForm.addEventListener('submit', function (event) {
+
+const galleryContainer = document.querySelector('.gallery');
+
+const searchForm = document.querySelector('.search-form');
+
+const loaderContainer = document.querySelector('.loader');
+
+const GALLERY_LINK = 'gallery-link';
+
+searchForm.addEventListener('submit', function (event) {
   event.preventDefault();
   const queryInput = event.target.elements.query.value;
 
@@ -15,29 +21,110 @@ refs.searchForm.addEventListener('submit', function (event) {
     return;
   }
 
-  refs.galleryContainer.innerHTML = '';
-  refs.loaderContainer.style.display = 'block';
+  galleryContainer.innerHTML = '';
+  loaderContainer.style.display = 'block';
 
   fetchImages(queryInput)
-    .then(function ({hits, total}) {
+    .then(function ({ hits, total }) {
       if (Array.isArray(hits) && hits.length > 0) {
         const galleryHTML = hits.map(createGallery).join('');
-        refs.galleryContainer.innerHTML = galleryHTML;
-        
-        toastSuccess(`Was found: ${total} images`)
+        galleryContainer.innerHTML = galleryHTML;
+
+        toastSuccess(`Was found: ${total} images`);
 
         const lightbox = new SimpleLightbox(`.${GALLERY_LINK}`);
-         
+
         lightbox.refresh();
       } else {
-        toastError('Sorry, there are no images matching your search query. Please try again!')
+        toastError(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
       }
     })
     .catch(function (error) {
-      toastError(`Error fetching images: ${error}`)
+      toastError(`Error fetching images: ${error}`);
     })
     .finally(function () {
-      refs.searchForm.reset();
-      refs.loaderContainer.style.display = 'none';
+      searchForm.reset();
+      loaderContainer.style.display = 'none';
     });
 });
+
+const toastOptions = {
+  titleColor: '#FFFFFF',
+  messageColor: '#FFFFFF',
+  messageSize: '16px',
+  position: 'topRight',
+  displayMode: 'replace',
+  closeOnEscape: true,
+  pauseOnHover: false,
+  maxWidth: 432,
+  messageSize: '16px',
+  messageLineHeight: '24px',
+};
+
+function toastError(message) {
+  iziToast.show({
+    message,
+    backgroundColor: '#EF4040',
+    progressBarColor: '#FFE0AC',
+    icon: 'icon-close',
+    ...toastOptions,
+  });
+}
+
+function toastSuccess(message) {
+  iziToast.show({
+    message,
+    backgroundColor: '#59A10D',
+    progressBarColor: '#B5EA7C',
+    icon: 'icon-chek',
+    ...toastOptions,
+  });
+}
+
+const BASE_URL = 'https://pixabay.com/api/';
+
+function fetchImages(q) {
+  const searchParams = new URLSearchParams({
+    key: '42137546-386b5be41212ccd429cab5a80',
+    q,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safeSearch: true,
+  });
+
+  const PARAMS = `?${searchParams}`;
+  const url = BASE_URL + PARAMS;
+
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => {
+      toastError(`Error fetching images: ${error}`);
+      throw error;
+    });
+}
+
+function createGallery({
+  largeImageURL,
+  tags,
+  webformatURL,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `
+  <a href="${largeImageURL}" class="${GALLERY_LINK}">
+     <figure>
+      <img src="${webformatURL}" alt="${tags}" class="gallery-image">
+      <figcaption class="gallery__figcaption">
+        <div class="image-item">Likes <span class="image-elem">${likes}</span></div>
+        <div class="image-item">Views <span class="image-elem">${views}</span></div>
+        <div class="image-item">Comments <span class="image-elem">${comments}</span></div>
+        <div class="image-item">Downloads <span class="image-elem">${downloads}</span></div>
+  </figcaption>
+  </figure>
+</a>
+`;
+}
